824ef5e1788d58523575b56ef15b898592a76e4b
Matt Del Signore <mattdelsig@me.com>
2020-05-05 21:42:39 -0400

    Added buefy, the composition api, and made the game page.

---
+++ src/components/CreateGame.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-tabs>
+    <b-tab-item label="Join a Game">
+      <b-field label="Game ID">
+        <b-input></b-input>
+      </b-field>
+      <b-button type="is-primary">Join Game</b-button>
+    </b-tab-item>
+
+    <b-tab-item label="Create Game">
+      <b-field label="Player Name">
+        <b-input></b-input>
+      </b-field>
+      <b-button type="is-primary">Create Game</b-button>
+    </b-tab-item>
+  </b-tabs>
+</template>
+
+<script lang="ts">
+import { createComponent, defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  setup: () => {
+    return {};
+  },
+});
+</script>
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import VueCompositionApi from '@vue/composition-api';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueCompositionApi);
+Vue.use(Buefy);
 
 new Vue({
   router,
