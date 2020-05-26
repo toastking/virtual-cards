631ccb8cfed168eecb1a2466fccecfcec78ec6a7
Matt Del Signore <mattdelsig@me.com>
2020-05-25 21:05:08 -0400

    added env file

--- src/db.ts
+++ src/db.ts
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 
 // Start up the firebase instance
 const firebaseConfig = {
-  apiKey: process.env.VUE_APP_API_KEY ?? '',
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN ?? '',
-  databaseURL: process.env.VUE_APP_DATABASE_URL ?? '',
-  projectId: process.env.VUE_APP_PROJECT_ID ?? '',
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET ?? '',
-  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID ?? '',
-  measurementId: process.env.VUE_APP_MEASUREMENT_ID ?? '',
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID,
 };
 export const app = firebase.initializeApp(firebaseConfig);
 export const db = app.firestore();
